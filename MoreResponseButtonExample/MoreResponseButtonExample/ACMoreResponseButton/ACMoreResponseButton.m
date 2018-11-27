e2bb7bd94673acae6d160e23ac69b4be5c16d67b
wangmengnan <acbool@lonlife.cn>
2018-11-27 21:28:41 +0800

    add ACMoreResponseButton

---
+++ MoreResponseButtonExample/MoreResponseButtonExample/ACMoreResponseButton/ACMoreResponseButton.m
@@ -0,0 +1,21 @@
+//
+//  ACMoreResponseButton.m
+//  MoreResponseButtonExample
+//
+//  Created by lonlife on 2018/11/27.
+//  Copyright © 2018 lonlife. All rights reserved.
+//
+
+#import "ACMoreResponseButton.h"
+
+@implementation ACMoreResponseButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
