5a7f4c29328b534c46a9dc89997d5ef32a0b4268
wangmengnan <acbool@lonlife.cn>
2018-11-27 18:21:11 +0800

    create new project

---
+++ MoreResponseButtonExample/MoreResponseButtonExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoreResponseButtonExample
+//
+//  Created by lonlife on 2018/11/27.
+//  Copyright © 2018 lonlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoreResponseButtonExample/MoreResponseButtonExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoreResponseButtonExample
+//
+//  Created by lonlife on 2018/11/27.
+//  Copyright © 2018 lonlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
